a8901c57e21bdf50285349ab153af38113c605c8
KeryaKeryaKerya100raz <132277602+KeryaKeryaKerya100raz@users.noreply.github.com>
2023-06-20 11:46:14 +0500

    dobavil redactirovanie

--- src/components/Active.js
+++ src/components/Active.js
@@ -1,14 +1,19 @@
-import React from 'react'
+
+import React, { useState } from 'react';
 
 function Active({ todo, setTodo }) {
   
+   const [edit, setEdit] = useState(null)
+   const [value, setValue] = useState('')
+   
     function deleteTodo(id) {
         let newTodo = [...todo].filter(item => item.id != id)
         setTodo(newTodo)
     }
   
-    function editTodo(){
-
+    function editTodo(id, title){
+        setEdit(id)
+        setValue(title)
     }
 
     function uraTodo(id){
@@ -22,16 +27,44 @@ function Active({ todo, setTodo }) {
 
     }
 
+    function saveTodo(id) {
+        let newTodo = [...todo].map( item => {
+            if (item.id == id ) {
+            item.title = value
+            }
+            return item
+        })
+            setTodo(newTodo)
+            setEdit(null)
+    }
+
     return (
     <div>
         <h3>Nado mutitsa</h3>
         {
             todo.map( item => (
                 <div key={item.id}>
-                <div>{item.title}</div>
-                <button onClick={ ()=>deleteTodo(item.id) }>Удалить</button>
-                <button onClick={ ()=>editTodo(item.id)}>Редактировать</button>
-                <button onClick={ ()=>uraTodo(item.id)}>Выполнено/Не выполнено</button>
+                    {
+                        edit == item.id ? 
+                            <div>
+                                <input onChange={(e) => setValue(e.target.value)} value={value} />
+                            </div>
+                                :
+                            <div>{ item.title}</div>
+                    }
+
+                    {
+                        edit == item.id ?
+                            <div><button onClick={() => saveTodo(item.id)} >Готово</button></div>
+                            :
+                            <div>
+                                <button onClick={ ()=>deleteTodo(item.id) }>Удалить</button>
+                                <button onClick={ ()=>editTodo(item.id, item.title)}>Редактировать</button>
+                                <button onClick={ ()=>uraTodo(item.id)}>Выполнено/Не выполнено</button>
+                            </div>
+                    }
+                   
+                    
                 </div>
             ))
         }
